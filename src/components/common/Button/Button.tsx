e462669ce668beffad5d0a9fca01089aa1badee7
matmal <m.mateusz2000@wp.pl>
2024-04-24 11:11:47 +0200

    feat: add tawlind merge

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,4 +1,5 @@
 import { ButtonHTMLAttributes, FC, HTMLProps, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 import cn from "classnames";
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -15,8 +16,10 @@ export const Button: FC<IButtonProps> = ({ text, onClick, variant = "primary", i
             {...props}
             onClick={onClick}
             className={cn(
-                "focus-visible:ring-primary focus-visible:ring-offset-background disabled:opacity-500 inline-flex h-10 w-full items-center justify-center whitespace-nowrap rounded-md  px-4 py-2 text-sm font-medium transition-colors  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none",
-                className,
+                twMerge(
+                    "focus-visible:ring-primary focus-visible:ring-offset-background disabled:opacity-500 inline-flex h-10 w-full items-center justify-center whitespace-nowrap rounded-md  px-4 py-2 text-sm font-medium transition-colors  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none",
+                    className
+                ),
                 {
                     "bg-black text-white hover:bg-gray-900": variant === "primary",
                     "border border-gray-300 bg-white text-black hover:bg-slate-50": variant === "secondary",
