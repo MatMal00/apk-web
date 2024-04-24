975d4c9ee141f264bad821947cadb5c8598720c8
matmal <m.mateusz2000@wp.pl>
2024-04-24 11:27:54 +0200

    feat: handle close modal on click outside

--- src/components/routes/Home/components/ProjectCard/KebabMenu/KebabMenu.tsx
+++ src/components/routes/Home/components/ProjectCard/KebabMenu/KebabMenu.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical, faTrash } from "@fortawesome/free-solid-svg-icons";
 
@@ -6,8 +6,23 @@ interface IKebabMenuProps {}
 
 export const KebabMenu: FC<IKebabMenuProps> = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const modalRef = useRef<HTMLDivElement | null>(null);
 
     const handleToggleKebabMenu = () => setIsOpen((prev) => !prev);
+
+    const handleDeleteProject = () => {
+        handleToggleKebabMenu();
+        console.log("DELETE");
+    };
+
+    const handleClickOutside = useCallback((event: MouseEvent) => {
+        if (modalRef.current && !modalRef.current.contains(event?.target as Node)) setIsOpen(false);
+    }, []);
+
+    useEffect(() => {
+        if (isOpen) document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, [isOpen, handleClickOutside]);
 
     return (
         <div className="relative">
@@ -18,9 +33,12 @@ export const KebabMenu: FC<IKebabMenuProps> = () => {
                 <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
             {isOpen && (
-                <div className="absolute right-0  w-40 origin-top-right overflow-hidden rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div
+                    ref={modalRef}
+                    className="absolute right-0  w-40 origin-top-right overflow-hidden rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
                     <button
-                        onClick={handleToggleKebabMenu}
+                        onClick={handleDeleteProject}
                         className="flex w-full items-center justify-between px-4 py-3 text-left text-sm text-gray-700 transition-colors hover:bg-red-900 hover:text-gray-900"
                         role="menuitem"
                     >
