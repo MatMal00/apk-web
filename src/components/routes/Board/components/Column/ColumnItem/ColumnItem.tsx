957d77f7d4de4a99398481caa27634a2030c19c0
matmal <m.mateusz2000@wp.pl>
2024-04-29 20:28:36 +0200

    style: column items

--- src/components/routes/Board/components/Column/ColumnItem/ColumnItem.tsx
+++ src/components/routes/Board/components/Column/ColumnItem/ColumnItem.tsx
@@ -3,6 +3,7 @@ import ReactTimeAgo from "react-time-ago";
 import { Modal } from "src/components/common";
 import { TASK_PRIORITY, TASK_STATUS } from "src/constants";
 import { TStory, TTask } from "src/types";
+import cn from "classnames";
 
 interface IColumnItemProps {
     modalContent: ReactNode | ReactNode[];
@@ -17,32 +18,38 @@ export const ColumnItem: FC<IColumnItemProps> = ({
     isModalOpen,
     task: { name, description, priority, status, dateAdded, estimatedCompletionTime },
 }) => {
+    // TEXT
     const priorityText = {
         [TASK_PRIORITY.LOW]: "Low",
         [TASK_PRIORITY.MEDIUM]: "Medium",
         [TASK_PRIORITY.HIGH]: "High",
     };
-    const statusText = {
-        [TASK_STATUS.TO_DO]: "to do",
-        [TASK_STATUS.DOING]: "doing",
-        [TASK_STATUS.DONE]: "done",
-    };
 
+    const isDone = status === TASK_STATUS.DONE;
     const completionTime = estimatedCompletionTime > 1 ? "hours" : "hour";
     return (
         <li className="cursor-pointer space-y-4" onClick={toggleModal}>
             <div className="rounded-lg bg-white p-4 shadow dark:bg-gray-700">
                 <div className="flex items-start justify-between">
                     <div>
-                        <h3 className="text-lg font-semibold">{name}</h3>
+                        <h3
+                            className={cn("text-lg font-semibold", {
+                                "line-through": isDone,
+                            })}
+                        >
+                            {name}
+                        </h3>
                         <p className="text-gray-500 dark:text-gray-400">{description}</p>
                     </div>
                     <div className="flex items-center space-x-2">
-                        <span className="rounded-full bg-yellow-100 px-2 py-1 text-xs font-medium text-yellow-800 dark:bg-yellow-800 dark:text-yellow-100">
+                        <span
+                            className={cn("rounded-full px-2 py-1 text-xs font-medium", {
+                                ["bg-gray-100 text-gray-800"]: priority === TASK_PRIORITY.LOW,
+                                ["bg-green-100 text-green-800"]: priority === TASK_PRIORITY.MEDIUM,
+                                ["bg-red-200 text-red-900"]: priority === TASK_PRIORITY.HIGH,
+                            })}
+                        >
                             {priorityText[priority]}
-                        </span>
-                        <span className="whitespace-nowrap rounded-full bg-gray-100 px-2 py-1 text-xs font-medium text-gray-800 dark:bg-gray-600 dark:text-gray-100">
-                            {statusText[status]}
                         </span>
                     </div>
                 </div>
