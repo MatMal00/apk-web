6a0e2a8de5eb977247be2e45249073ba0b34cbaf
matmal <m.mateusz2000@wp.pl>
2024-05-04 15:19:32 +0200

    ref: add task & story

--- src/components/routes/Board/components/StoriesList/components/Search/SearchStories/AddStoryForm/AddStoryForm.tsx
+++ src/components/routes/Board/components/StoriesList/components/Search/SearchStories/AddStoryForm/AddStoryForm.tsx
@@ -1,76 +1,64 @@
 import { FC } from "react";
 import { Button, Card, Input, TextArea } from "src/components/common";
 import { Form, Formik } from "formik";
-import { useAuth } from "src/hooks";
+import { PriorityDropdown } from "src/components/common/Form/Dropdown/components";
+import { TASK_PRIORITY, TASK_STATUS } from "src/constants";
 import { TStory } from "src/types";
-import { createProject } from "src/helpers";
-import { TASK_PRIORITY } from "src/constants";
 
-interface IAddStoryFormProps {
+interface IAddStoryFormFormProps {
     close: () => void;
     addNewStory: (newStory: Omit<TStory, "uid">) => Promise<void>;
 }
 
-export const AddStoryForm: FC<IAddStoryFormProps> = ({ close, addNewStory }) => {
-    const { currentUser } = useAuth();
-
-    const handleAddNewStory = () => {
-        if (currentUser) {
-            const payload: Omit<TStory, "uid"> = {
-                name: "Ultrasonic Drill Rig",
-                description:
-                    "This advanced drill rig offers high-efficiency and precision for complex geological surveys.",
-                priority: TASK_PRIORITY.MEDIUM,
-                estimatedCompletionTime: 0,
-                status: "doing",
-                dateAdded: new Date().getTime(),
-                assignedUser: {
-                    uid: currentUser.uid,
-                    name: currentUser.username,
-                    role: currentUser.role,
-                },
-                tasks: [],
-            };
-
-            addNewStory(payload);
-            close();
-        }
-    };
-
+export const AddStoryForm: FC<IAddStoryFormFormProps> = ({ close, addNewStory }) => {
     return (
-        <Card className="w-full max-w-lg cursor-default rounded-lg bg-white p-4 shadow-xl md:p-0">
+        <Card className="w-full cursor-default rounded-lg bg-white p-4 shadow-xl md:p-0">
             <Card.Header className="border-b pb-4">
-                <h3 className="text-2xl font-semibold">Add New Project</h3>
+                <h3 className="text-2xl font-semibold">Add new story</h3>
             </Card.Header>
             <Formik
-                validationSchema={createProject}
                 initialValues={{
-                    name: "Project",
-                    description:
-                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id incidunt esse est architecto voluptatibus excepturi",
-                    logoUrl:
-                        "https://img.freepik.com/free-photo/light-bulb-with-drawing-graph_1232-2105.jpg?size=626&ext=jpg",
+                    name: "",
+                    description: "",
+                    priority: TASK_PRIORITY.LOW,
                 }}
-                onSubmit={() => handleAddNewStory()}
+                onSubmit={(values) => {
+                    addNewStory({
+                        ...values,
+                        dateAdded: new Date().getTime(),
+                        status: TASK_STATUS.TO_DO,
+                        estimatedCompletionTime: 0,
+                        tasks: [],
+                    });
+                    close();
+                }}
             >
-                <Form>
-                    <Card.Content className="block py-4">
-                        <div className="flex flex-col space-y-4">
-                            <Input name="name" label="Project Name" placeholder="Enter the project name" />
-                            <Input name="logoUrl" label="Logo url" placeholder="Enter the logo url" />
-                            <TextArea
-                                className="min-h-28"
-                                name="description"
-                                label="Description"
-                                placeholder="Describe the project"
+                {({ values: { priority }, dirty }) => (
+                    <Form>
+                        <Card.Content className="block py-4">
+                            <div className="flex max-h-72 flex-col space-y-4 overflow-y-auto pr-2.5 md:max-h-max md:p-2">
+                                <Input name="name" label="Story Name" placeholder="Enter the project name" />
+                                <PriorityDropdown priority={priority} />
+                                <TextArea
+                                    className="min-h-28"
+                                    name="description"
+                                    label="Description"
+                                    placeholder="Describe the project"
+                                />
+                            </div>
+                        </Card.Content>
+                        <Card.Footer className="flex justify-start gap-5 border-t pt-4">
+                            <Button
+                                onClick={close}
+                                type="button"
+                                className="basis-32"
+                                variant="secondary"
+                                text="Close"
                             />
-                        </div>
-                    </Card.Content>
-                    <Card.Footer className="flex justify-between gap-5 border-t pt-4">
-                        <Button type="button" onClick={close} variant="secondary" text="Cancel" />
-                        <Button type="submit" className="ml-2" text="Add Project" />
-                    </Card.Footer>
-                </Form>
+                            <Button type="submit" className="ml-2 basis-32" text="Save" disabled={!dirty} />
+                        </Card.Footer>
+                    </Form>
+                )}
             </Formik>
         </Card>
     );
--- src/components/routes/Board/components/StoriesList/components/Search/SearchTasks/AddTaskForm/AddTaskForm.tsx
+++ src/components/routes/Board/components/StoriesList/components/Search/SearchTasks/AddTaskForm/AddTaskForm.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react";
 import { Button, Card, Input, TextArea } from "src/components/common";
 import { Form, Formik } from "formik";
-import { useAuth } from "src/hooks";
 import { TTask } from "src/types";
-import { createProject } from "src/helpers";
-import { TASK_PRIORITY } from "src/constants";
+import { PriorityDropdown } from "src/components/common/Form/Dropdown/components";
+import { TASK_PRIORITY, TASK_STATUS } from "src/constants";
 
 interface IAddTaskFormProps {
     close: () => void;
@@ -13,65 +12,66 @@ interface IAddTaskFormProps {
 }
 
 export const AddTaskForm: FC<IAddTaskFormProps> = ({ close, addNewTask, storyUid }) => {
-    const { currentUser } = useAuth();
-
-    const handleAddNewTask = () => {
-        if (currentUser) {
-            const payload: Omit<TTask, "uid"> = {
-                name: "Integrate Payment Gateway",
-                description:
-                    "Implement and configure the secure payment gateway integration for the e-commerce platform.",
-                priority: TASK_PRIORITY.MEDIUM,
-                storyUid,
-                estimatedCompletionTime: 40,
-                status: "todo",
-                dateAdded: new Date().getTime(),
-                assignedUser: {
-                    uid: "98zyx-54vu-321t-srqp-onml",
-                    name: "Samantha Reed",
-                    role: "DEV",
-                },
-            };
-
-            addNewTask(payload);
-            close();
-        }
-    };
-
     return (
-        <Card className="w-full max-w-lg cursor-default rounded-lg bg-white p-4 shadow-xl md:p-0">
+        <Card className="w-full cursor-default rounded-lg bg-white p-4 shadow-xl md:p-0">
             <Card.Header className="border-b pb-4">
-                <h3 className="text-2xl font-semibold">Add New Project</h3>
+                <h3 className="text-2xl font-semibold">Add new task</h3>
             </Card.Header>
             <Formik
-                validationSchema={createProject}
                 initialValues={{
-                    name: "Project",
-                    description:
-                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id incidunt esse est architecto voluptatibus excepturi",
-                    logoUrl:
-                        "https://img.freepik.com/free-photo/light-bulb-with-drawing-graph_1232-2105.jpg?size=626&ext=jpg",
+                    name: "",
+                    description: "",
+                    priority: TASK_PRIORITY.LOW,
+                    estimatedCompletionTime: 0,
                 }}
-                onSubmit={() => handleAddNewTask()}
+                onSubmit={(values) => {
+                    addNewTask({
+                        ...values,
+                        dateAdded: new Date().getTime(),
+                        status: TASK_STATUS.TO_DO,
+                        storyUid,
+                    });
+                    close();
+                }}
             >
-                <Form>
-                    <Card.Content className="block py-4">
-                        <div className="flex flex-col space-y-4">
-                            <Input name="name" label="Project Name" placeholder="Enter the project name" />
-                            <Input name="logoUrl" label="Logo url" placeholder="Enter the logo url" />
-                            <TextArea
-                                className="min-h-28"
-                                name="description"
-                                label="Description"
-                                placeholder="Describe the project"
-                            />
-                        </div>
-                    </Card.Content>
-                    <Card.Footer className="flex justify-between gap-5 border-t pt-4">
-                        <Button type="button" onClick={close} variant="secondary" text="Cancel" />
-                        <Button type="submit" className="ml-2" text="Add Project" />
-                    </Card.Footer>
-                </Form>
+                {({ values: { priority, estimatedCompletionTime }, dirty }) => {
+                    const completionTime = estimatedCompletionTime > 1 ? "hours" : "hour";
+
+                    return (
+                        <Form>
+                            <Card.Content className="block py-4">
+                                <div className="flex max-h-72 flex-col space-y-4 overflow-y-auto pr-2.5 md:max-h-max md:p-2">
+                                    <Input name="name" label="Story Name" placeholder="Enter the project name" />
+                                    <PriorityDropdown priority={priority} />
+                                    <TextArea
+                                        className="min-h-28"
+                                        name="description"
+                                        label="Description"
+                                        placeholder="Describe the project"
+                                    />
+                                    <div className="flex items-end gap-2">
+                                        <Input
+                                            name="estimatedCompletionTime"
+                                            type="number"
+                                            label="Estimated completion time"
+                                        />
+                                        <p>{completionTime}</p>
+                                    </div>
+                                </div>
+                            </Card.Content>
+                            <Card.Footer className="flex justify-start gap-5 border-t pt-4">
+                                <Button
+                                    onClick={close}
+                                    type="button"
+                                    className="basis-32"
+                                    variant="secondary"
+                                    text="Close"
+                                />
+                                <Button type="submit" className="ml-2 basis-32" text="Save" disabled={!dirty} />
+                            </Card.Footer>
+                        </Form>
+                    );
+                }}
             </Formik>
         </Card>
     );
