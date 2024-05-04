f769bfc6f6a75de7ca50aa833aad30115b2fd86d
matmal <m.mateusz2000@wp.pl>
2024-05-04 14:30:07 +0200

    ref: wrap modal in portal

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import { useHandleClickOutside } from "src/hooks/useHandleClickOutside";
+import { Portal } from "../Portal";
 import cn from "classnames";
 
 interface IModalProps {
@@ -14,16 +15,18 @@ export const Modal: FC<IModalProps> = ({ isOpen, close, large, children }) => {
 
     if (!isOpen) return null;
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div
-                ref={modalRef}
-                className={cn("w-full p-4", {
-                    "max-w-lg": !large,
-                    "max-w-screen-xl": large,
-                })}
-            >
-                {children}
+        <Portal>
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                <div
+                    ref={modalRef}
+                    className={cn("w-full p-4", {
+                        "max-w-lg": !large,
+                        "max-w-screen-xl": large,
+                    })}
+                >
+                    {children}
+                </div>
             </div>
-        </div>
+        </Portal>
     );
 };
