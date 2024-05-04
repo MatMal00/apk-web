19c40fa83a534e6e0410e0b3c2e8c49d1e8af99d
matmal <m.mateusz2000@wp.pl>
2024-05-04 13:19:12 +0200

    ref: remove redundant code

--- src/libs/old/useFetchAllUsers.ts
+++
@@ -1,8 +0,0 @@
-import SWR from "swr";
-import { fetcher } from "src/actions";
-import { IUser } from "src/types";
-
-export const useFetchAllUsers = () => {
-    const { data, error, isLoading } = SWR<IUser[], string>(`/users`, fetcher);
-    return { data, error, isLoading };
-};
--- src/libs/old/useFetchUserDetails.ts
+++
@@ -1,8 +0,0 @@
-import SWR from "swr";
-import { fetcher } from "src/actions";
-import { IUser } from "src/types";
-
-export const useFetchUserDetails = (userId: number) => {
-    const { data, error, isLoading } = SWR<IUser, string>(`/users/${userId}`, fetcher);
-    return { data, error, isLoading };
-};
